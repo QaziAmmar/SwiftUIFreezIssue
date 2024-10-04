ecc58b03a0cda5fbf3905d6f20079054cedf2f52
Qazi Ammar Arshad <qaziammar.g@gmail.com>
2024-10-04 13:06:36 +0500

    Initial Commit

---
+++ SwiftUIFreezIssue/SwiftUIFreezIssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFreezIssueApp.swift
+//  SwiftUIFreezIssue
+//
+//  Created by Qazi Ammar Arshad on 04/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFreezIssueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
